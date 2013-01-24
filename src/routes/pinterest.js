89aea38fdb63112b3711b689b32bfcf04bac59e9
jaketrent <trent.jake@gmail.com>
2013-01-23 20:06:59 -0700

    use superagent for pinterest req; log response

--- src/routes/pinterest.js
+++ src/routes/pinterest.js
@@ -1,23 +1,30 @@
 
-var request = require('request');
+var request = require('superagent');
 var cheerio = require('cheerio');
 
 exports.list = function (req, res) {
-  request('http://pinterest.com/jaketrent/pins/', function(err, resp, body){
-    var $ = cheerio.load(body);
-    var pins = [];
-    var $pin;
-    var $pins = $('.pin');
 
-    var i = 0;
-    for (i; i < 10; ++i) {
-      $pin = $pins.eq(i);
-      pins.push({
-        img: $pin.find('.PinImageImg').attr('src'),
-        url: 'http://pinterest.com' + $pin.find('.ImgLink').attr('href'),
-        desc: $pin.find('.description').text()
-      });
-    }
-    res.send(pins);
-  });
+  request
+    .get('http://pinterest.com/jaketrent/pins/')
+    .end(function(response){
+
+      console.log('pinterest ws status: ' + response.statusCode);
+      console.log(response.text);
+
+      var $ = cheerio.load(response.text);
+      var pins = [];
+      var $pin;
+      var $pins = $('.pin');
+
+      var i = 0;
+      for (i; i < 10; ++i) {
+        $pin = $pins.eq(i);
+        pins.push({
+          img: $pin.find('.PinImageImg').attr('src'),
+          url: 'http://pinterest.com' + $pin.find('.ImgLink').attr('href'),
+          desc: $pin.find('.description').text()
+        });
+      }
+      res.send(pins);
+    });
 };
